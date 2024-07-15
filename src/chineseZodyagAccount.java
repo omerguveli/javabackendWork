c04110a1c5fe9967d914daa548d2a309b4708ac6
Ofguveli <ofguveli@newmind.ai>
2024-07-15 13:21:15 +0300

    chineseZodyagAccount

---
+++ src/chineseZodyagAccount.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class chineseZodyagAccount {
+    public static void main(String[] args) {
+        int birthDate;
+        Scanner zodyag = new Scanner(System.in);
+        System.out.print("doğum tarihini giriniz : ");
+        birthDate = zodyag.nextInt();
+        switch (birthDate % 12) {
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz: Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz: Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz: Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz: Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz: Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz: Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz: Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz: Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz: Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz: Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz: At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz: Koyun");
+                break;
+            default:
+                System.out.println("Geçersiz doğum tarihi");
+        }
+    }
+}
