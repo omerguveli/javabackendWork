6efaeb0e79e9df64f0c057703040e45b5e1cb92c
Ofguveli <ofguveli@newmind.ai>
2024-07-15 14:22:06 +0300

    artikYil

---
+++ src/artikYilHesaplama.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class artikYilHesaplama {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter year: ");
+        int year = scanner.nextInt();
+        String result = "";
+        if(year % 100 == 0) {
+            if (year % 400 == 0) {
+                result = year + " is leap year";
+            } else {
+                result = year + "is not leap year";
+            }
+        } else if (year % 4 == 0) {
+            result = year + " is leap year";
+        } else {
+            result = year + "is not leap year";
+        }
+        System.out.println(result);
+    }
+}
